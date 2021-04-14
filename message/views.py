be6b77d21db0478f3a1e09d4d072ae8d0d774236
win21g <win21g@mail.ru>
2021-04-14 22:03:37 +0300

    new main

--- message/views.py
+++ message/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import *
-from rest_framework import viewsets
+from rest_framework import viewsets,permissions
 from message.serializers import *
 # Create your views here.
 
