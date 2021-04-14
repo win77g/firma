991457df0d2be1cddcf35b69335b9652398cf3e7
win21g <win21g@mail.ru>
2021-04-14 22:07:33 +0300

    new main

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     # 'DEFAULT_PERMISSION_CLASSES':(
     #     # 'rest_framework.permissions.IsAdminUser',
-    #     # 'rest_framework.permissions.AllowAny',
+          'rest_framework.permissions.AllowAny',
     #     'rest_framework.permissions.IsAuthenticated',
     #                              ),
     # 'DEFAULT_AUTHENTICATION_CLASSES':(
